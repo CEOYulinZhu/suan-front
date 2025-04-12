2419023468ecb85f80208be158749c0f94d7d846
CEOYuLinZhu <3159268198@qq.com>
2025-04-12 18:10:40 +0800

    实现登录页面

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,14 +5,97 @@ Page({
    * 页面的初始数据
    */
   data: {
+    username: '',
+    password: '',
+    rememberMe: false
+  },
 
+  /**
+   * 输入用户名
+   */
+  usernameInput: function (e) {
+    this.setData({
+      username: e.detail.value
+    });
+  },
+
+  /**
+   * 输入密码
+   */
+  passwordInput: function (e) {
+    this.setData({
+      password: e.detail.value
+    });
+  },
+
+  /**
+   * 记住我切换
+   */
+  toggleRememberMe: function () {
+    this.setData({
+      rememberMe: !this.data.rememberMe
+    });
+  },
+
+  /**
+   * 登录操作
+   */
+  login: function () {
+    const { username, password } = this.data;
+    if (!username || !username.trim()) {
+      wx.showToast({
+        title: '请输入用户名',
+        icon: 'none'
+      });
+      return;
+    }
+    if (!password || !password.trim()) {
+      wx.showToast({
+        title: '请输入密码',
+        icon: 'none'
+      });
+      return;
+    }
+
+    // 这里添加实际登录逻辑
+    console.log('登录信息:', this.data);
+    wx.showToast({
+      title: '登录成功',
+      icon: 'success',
+      duration: 1000
+    });
+
+    // 登录成功后跳转到首页
+    setTimeout(() => {
+      wx.switchTab({
+        url: '/pages/index/index',
+      });
+    }, 1000);
+  },
+
+  /**
+   * 跳转到注册页面
+   */
+  goToRegister: function () {
+    wx.navigateTo({
+      url: '/pages/register/register',
+    });
+  },
+
+  /**
+   * 跳转到忘记密码页面
+   */
+  goToForgotPassword: function () {
+    wx.navigateTo({
+      url: '/pages/forgot-password/forgot-password',
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    // 可以在这里做一些初始化工作
   },
 
   /**
