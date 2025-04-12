c81dd2cc2b0c046559bce73682e86df50f11e400
CEOYuLinZhu <3159268198@qq.com>
2025-04-12 13:59:27 +0800

    完成按钮组件内容

---
+++ components/custom-button/custom-button.js
@@ -0,0 +1,26 @@
+Component({
+    properties: {
+        // 按钮类型，可选值为 'primary' 或 'secondary'
+        type: {
+            type: String,
+            value: 'primary' // 默认主按钮样式
+        },
+        // 按钮文字
+        text: {
+            type: String,
+            value: '按钮'
+        },
+        // 点击事件的回调函数名
+        bindtap: {
+            type: String,
+            value: ''
+        }
+    },
+
+    methods: {
+        handleTap() {
+            // 触发父组件绑定的点击事件
+            this.triggerEvent('buttontap');
+        }
+    }
+}); 
